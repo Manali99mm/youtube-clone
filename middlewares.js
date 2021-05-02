ceff7d5b2b460e65371bfbde0133da80987933d8
Manali Mathur <a.manalimathur@gmail.com>
2021-05-02 17:22:14 +0530

    SCSS

--- middlewares.js
+++ middlewares.js
@@ -6,7 +6,7 @@ const multerVideo = multer({ dest: 'uploads/videos/' });
 export const localsMiddleware = (req, res, next) => {
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated: true,
+        isAuthenticated: false,
         id: 1
     }
     next();
