dea957b6e613564d3635107712504779eece868c
Manali Mathur <a.manalimathur@gmail.com>
2021-05-18 01:23:52 +0530

    User Profile and Change Password

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { changePassword, editProfile, userDetail } from "../controllers/userController.js";
-import { onlyPrivate } from "../middlewares.js";
+import { getChangePassword, getEditProfile, postChangePassword, postEditProfile, userDetail } from "../controllers/userController.js";
+import { onlyPrivate, uploadAvatar } from "../middlewares.js";
 import routes from "../routes.js";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile)
-userRouter.get(routes.changePassword, onlyPrivate, changePassword)
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile)
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile)
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword)
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword)
 userRouter.get(routes.userDetail(), userDetail)
 
 export default userRouter;
